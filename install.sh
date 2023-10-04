5585c6d1ace8377662fd7780be5c6731461eeb3c
DomenickD3 <11238549+DomenickD3@users.noreply.github.com>
2023-10-03 23:57:30 -0400

    refactor(main.yml): Separate main.yml into install.yml and configure.yml
    install requires sudo
    configure does not require sudo
    .. these roles were in conflict, and should use separate playbooks

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt-get -y update
+
+sudo apt-get -y install git
+sudo apt-get -y install ansible 
+ 
+base_dir="$HOME/Documents/getting-started"
+git clone https://github.com/DomenickD3/getting-started.git $base_dir
+
+ansible_dir="$base_dir/ansible"
+sudo ansible-playbook -i "$ansible_dir/hosts" "$ansible_dir/install.yml" -vv
+ansible-playbook -i "$ansible_dir/hosts" "$ansible_dir/configure.yml" -vv
